fff739ebd31639596615900896873ad65f388af0
chelofabian <chelito.fabian.a@gmail.com>
2025-04-30 21:01:51 -0300

    Corrección funcion reducción de niveles

--- procesamiento.py
+++ procesamiento.py
@@ -1,6 +1,6 @@
 from skimage.io import imread
-from skimage.color import rgb2gray
 import matplotlib.pyplot as plt
+import numpy as np
 
 class Imagen:
     def __init__(self, ruta):
@@ -16,13 +16,16 @@ class Imagen:
         plt.imsave(nombre_archivo, self.imagen)
 
     def convertir_a_grises(self):
+        from skimage.color import rgb2gray
         self.imagen = rgb2gray(self.imagen)
 
     def reducir_niveles(self, niveles):
-        factor = 256 // niveles
-        imagen_reescalada = (self.imagen // factor) * factor
-        self.imagen = imagen_reescalada
-    
+        if self.imagen.dtype == np.float64:
+            imagen_reducida = np.floor(self.imagen * niveles) / niveles
+        else:  # Asumiendo tipo entero (por ejemplo, uint8)
+            imagen_reducida = (self.imagen // niveles) * niveles
+        self.imagen = imagen_reducida.astype(self.imagen.dtype)   
+
     def detectar_bordes(self, umbral_minimo, umbral_maximo):
         from skimage import feature
         self.imagen = feature.canny(self.imagen, sigma=1, low_threshold=umbral_minimo, high_threshold=umbral_maximo)
@@ -39,7 +42,7 @@ imagen.mostrar()
 
 imagen.guardar('transformadas/imagen_grises_skimage.jpg')
 
-imagen.reducir_niveles(8)
+imagen.reducir_niveles(4)
 imagen.mostrar()
 
 imagen.guardar('transformadas/imagen_grises_skimage_2.jpg')
