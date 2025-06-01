abb0f81b15e5271ed2860f27c10d7008c91a4e55
chelofabian <chelito.fabian.a@gmail.com>
2025-06-01 19:23:50 -0300

    Reestructuración de carpetas

--- app/main.py
+++ app/main.py
@@ -3,12 +3,13 @@ from fastapi.responses import StreamingResponse
 import io # Para BytesIO (IMAGEN EN MEMORIA)
 from skimage import io as skio # Para leer y guardar la imagen
 import numpy as np
-from app.procesamiento.imagenes import ProcesarImagen
+from procesamiento.imagenes import ProcesarImagen
+import ast
 
 app = FastAPI()
 
 @app.get("/")
-async def root():
+async def principal():
     return {"message": "Bienvenido a la API de procesamiento de imágenes. Usa /procesar-imagen/ para procesar una imagen, o bien ingresá a /docs para ver la documentación interactiva."}
 
 @app.post("/procesar-imagen/")
@@ -22,13 +23,31 @@ async def procesar_la_imagen(file: UploadFile = File(...), escala: float = 1.0, 
     Imagen = ProcesarImagen(imagen_array)
 
     # Aplica los métodos de procesamiento que definimos en la clase ProcesarImagen, aca hay que redefinir la forma en que pasamos la mascara porque antes habiamos definido una interfase gráfica...
-    Imagen.reescalar(escala)
-    #Mascara = SeleccionarMascara(Imagen.imagen) -> Viejo
-    #Imagen.mask = Mascara.mask -> Viejo
+    # Imagen.reescalar(escala)
+
+    if mascara is not None:
+        # Convierte la cadena de la máscara en un array de booleanos
+        try:
+            mascara_lista = ast.literal_eval(mascara)
+            mascara_array = np.array(mascara_lista, dtype=bool)
+            # Redimensiona la máscara si es necesario
+            if mascara_array.shape != Imagen.imagen.shape[:-1]:
+                mascara_array = np.resize(mascara_array, Imagen.imagen.shape[:-1])
+            Imagen.mascara = mascara_array
+        except Exception as e:
+            raise ValueError(f"Error al procesar la máscara: {e}")
+    else:
+        Imagen.mascara = np.zeros(Imagen.imagen.shape[:-1], dtype=bool)
+
+
+    # Pasos especificos de procesamiento ################ -> Esto capaz hay que meterlo en otra clase pero por ahora lo dejamos asi
+
     Imagen.quitar_ruido(sigma)
     Imagen.restaurar()
-    #procesador.aplicar() -> No la aplicamos aun porque como no hay mascar aun se veria igual que la original
+    Imagen.quitar_ruido(sigma)
+    Imagen.aplicar() 
 
+    # fin pasos especificos de procesamiento ############
 
     # Convierte la imagen procesada que viene como array a uint8 (que es el clasico 0-255) para que el output sea una imagen normal
     imagen_procesada_uint8 = (Imagen.imagen * 255).astype(np.uint8)
